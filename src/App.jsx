d8a33cb0640aee3255f6b01f471404600a213293
Sakibur59 <sakiburrahman5978@gmail.com>
2026-03-16 21:29:59 +0600

    Install Tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-4xl text-green-800">Explore Tailwind CSS in react</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
