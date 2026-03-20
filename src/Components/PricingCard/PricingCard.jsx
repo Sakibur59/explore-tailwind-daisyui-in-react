171c6a41ff6390712e338976e08f3518c20aa15e
Sakibur59 <sakiburrahman5978@gmail.com>
2026-03-20 19:50:29 +0600

    listed features item

--- src/Components/PricingCard/PricingCard.jsx
+++ src/Components/PricingCard/PricingCard.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import PricingFeature from '../PricingFeature/PricingFeature';
 
 const PricingCard = ({pricing}) => {
-    console.log(pricing)
+    const {name,price,features} = pricing
+    
     return (
         <div className='border bg-amber-500 rounded-md'>
             <div>
-                <h1 className='text-4xl'>{pricing.name}</h1>
-                <h4 className='text-2xl'>{pricing.price}</h4>
+                <h1 className='text-4xl'>{name}</h1>
+                <h4 className='text-2xl'>{price}</h4>
             </div>
             <div>
-                <p>{pricing.features}</p>
+                {
+                    features.map(feature => <PricingFeature feature={feature}></PricingFeature>)
+                }
             </div>
         </div>
     );
