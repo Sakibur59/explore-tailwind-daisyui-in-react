7abbc5dc1db4a763637ba02ac80cddaf80803e2e
Sakibur59 <sakiburrahman5978@gmail.com>
2026-03-20 20:21:16 +0600

    circle check icon and discription are added

--- src/Components/PricingCard/PricingCard.jsx
+++ src/Components/PricingCard/PricingCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PricingFeature from '../PricingFeature/PricingFeature';
 
 const PricingCard = ({pricing}) => {
-    const {name,price,features} = pricing
+    const {name,price,description,features} = pricing
     
     return (
         <div className='border bg-amber-500 rounded-md'>
@@ -11,8 +11,9 @@ const PricingCard = ({pricing}) => {
                 <h4 className='text-2xl'>{price}</h4>
             </div>
             <div>
+                <p>{description}</p>
                 {
-                    features.map(feature => <PricingFeature feature={feature}></PricingFeature>)
+                    features.map((feature,index) => <PricingFeature key={index} feature={feature}></PricingFeature>)
                 }
             </div>
         </div>
