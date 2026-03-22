1bd2db6a06c451fa3d5736971d9a2ca7c83a67cc
Sakibur59 <sakiburrahman5978@gmail.com>
2026-03-22 17:25:48 +0600

    converting pricing card to daisy pricing card

--- src/Components/PricingOption/PricingOption.jsx
+++ src/Components/PricingOption/PricingOption.jsx
@@ -1,5 +1,6 @@
 import React, { use } from 'react';
 import PricingCard from '../PricingCard/PricingCard';
+import DaisyPricingCard from '../DaisyPricingCard/DaisyPricingCard';
 
 const PricingOption = ({pricingPromise}) => {
     const pricingData = use(pricingPromise);
@@ -9,10 +10,12 @@ const PricingOption = ({pricingPromise}) => {
             <h2>Get Our Membership</h2>
             <div  className='grid md:grid-cols-2 lg:grid-cols-3 gap-8'>
 
-            {
+            {/* {
                 pricingData.map(pricing => <PricingCard key={pricing.id} pricing={pricing}></PricingCard>)
+            } */}
+            {
+                pricingData.map(pricing => <DaisyPricingCard key={pricing.id} pricing={pricing}></DaisyPricingCard>)
             }
-            
             </div>
         </div>
     );
