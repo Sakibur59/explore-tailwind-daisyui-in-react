d599ba864a9cde9ac66d42d759812718508b2e8e
Sakibur59 <sakiburrahman5978@gmail.com>
2026-03-22 17:06:54 +0600

    make responsive for medium and large device

--- src/Components/PricingOption/PricingOption.jsx
+++ src/Components/PricingOption/PricingOption.jsx
@@ -7,7 +7,7 @@ const PricingOption = ({pricingPromise}) => {
     return (
         <div>
             <h2>Get Our Membership</h2>
-            <div  className='grid md:grid-cols-3 gap-8'>
+            <div  className='grid md:grid-cols-2 lg:grid-cols-3 gap-8'>
 
             {
                 pricingData.map(pricing => <PricingCard key={pricing.id} pricing={pricing}></PricingCard>)
